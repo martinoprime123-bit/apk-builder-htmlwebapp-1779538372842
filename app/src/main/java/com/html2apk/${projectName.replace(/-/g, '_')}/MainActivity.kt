8f9c532c3bacbcb6f23aac6fdd3db644037c9c28
martinoprime123-bit <martinoprime123@gmail.com>
2026-05-23 19:13:03 +0700

    Add Android WebView App and HTML asset

---
+++ app/src/main/java/com/html2apk/${projectName.replace(/-/g, '_')}/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.html2apk.htmlwebapp
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView: WebView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
